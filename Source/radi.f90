27796ee5b39ba85db33c0bce3f3df3bd15f72bd4
ericvmueller <ericvmueller@gmail.com>
2025-11-21 14:25:34 -0500

    FDS Source: add diamond difference scheme to RTE for testing

--- Source/radi.f90
+++ Source/radi.f90
@@ -4283,6 +4283,8 @@ BAND_LOOP: DO IBND = 1,NUMBER_SPECTRAL_BANDS
                         SELECT CASE(RADIATION_SCHEME)
                             CASE DEFAULT ! Upwind (step)
                                 FWX = 1._EB; FWY = 1._EB; FWZ = 1._EB
+                            CASE(2) ! diamond
+                                FWX = 0.5_EB; FWY = 0.5_EB; FWZ = 0.5_EB
                             CASE(4) ! exponential v2
                                 PLX(:) = HUGE_EB
                                 IF(ABS(DLANG(1,N))>0._EB) PLX(1) = DX(I)/ABS(DLANG(1,N))
